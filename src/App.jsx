5caef580da5f6f7a046e81c529b7044d69967658
Ashley Morgan <ashleymorgan19901023@gmail.com>
2022-12-13 14:23:35 -0700

    fixed bugs to allow variables to work with vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
-import { useState } from 'react'
+
+import { useEffect, useState } from 'react'
+import supabase from './supabaseClient';
 
 function App() {
+
+  const[fetchError, setError] = useState(null);
+  const [paints, setPaints] = useState(null);
+
+  useEffect(() => {
+    const fetchPaints = async () => {
+      let { data: citadel_air, error } = await supabase
+        .from('citadel_air')
+        .select()
+  
+      if (error) {
+        setError('Could not GET paints');
+        setPaints(null);
+        console.log(error);
+      }
+  
+      if (citadel_air) {
+        setPaints(citadel_air);
+        setError(null);
+      }
+    }
+  
+    fetchPaints();
+  }, [])
   
   return (
-    <h1 className='text-center text-4xl'>React App Paint Library</h1>
+    <div>
+      <h1 className='text-center text-4xl'>React App Paint Library</h1>
+      {fetchError && (<h4 className="text-center text-2xl text-green-800">{fetchError}</h4>)}
+        {paints && (
+          <div className="text-center text-lg">
+            <h2 className="text-2xl">Paints</h2>
+            {paints.map(paint => (
+              <p>{paint.paint_name}</p>
+            ))}
+          </div>
+        )}
+    </div>
   )
 }
 

3ea5c501b8b43be62b2dcfcfc7d10cc5b7c8c2a0
Ashley Morgan <ashleymorgan19901023@gmail.com>
2022-12-14 20:05:01 -0700

    added fonts to nav

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -5,10 +5,10 @@ function TopNav() {
   return (
     <header className="text-darkblue dark:text-offwhite">
       <div className="mx-auto flex h-16 max-w-screen-xl items-center justify-around gap-8 px-4 sm:px-6 lg:px-8">
-        <Link to="/">The Paint Atheneum</Link>
+        <Link to="/" class="font-display text-xl md:text-2xl">The Paint Atheneum</Link>
         <div class="flex flex-1 items-center justify-end md:justify-between">
           <nav aria-label="Site Nav" class="hidden md:block">
-            <ul class="flex items-center gap-6">
+            <ul class="flex items-center gap-6 font-serif">
               <li>
                 <Link class="hover:bg-darkblue hover:text-offwhite hover:dark:bg-golden hover:dark:text-darkblue transition text-lg px-2 py-1" to="/">
                   Home

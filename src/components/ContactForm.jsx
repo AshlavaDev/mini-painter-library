994ac1df665ac5e38b70b5c1cb917d3502445e0d
Ashley Morgan <ashleymorgan19901023@gmail.com>
2022-12-16 11:42:18 -0700

    disabled form for netlify

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,7 +4,7 @@ function ContactForm() {
     <section className="mb-32 mt-12 text-center text-darkblue font-sans">
       <div className="max-w-[1/2] mx-auto px-3 lg:px-6">
         <h2 className="text-3xl font-serif mb-12">Contact us</h2>
-        <form name="contact" method="POST" data-netlify="true" className="w-full">
+        <form name="contact" method="POST" className="w-full">
           <div className="form-group mb-6 w-full">
             <input type="text" className="form-control block
               w-full
